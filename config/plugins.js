77ac2a9e519326a45014fe651e67e9727c5e3cca
jainChetan81 <jain.cj.chetan@gmail.com>
2022-01-09 21:52:35 +0530

    changed cloudinary with strapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,28 @@
+/*module.exports = ({ env }) => ({
+	upload: {
+		provider: "cloudinary",
+		providerOptions: {
+			cloud_name: env("CLOUDINARY_NAME"),
+			api_key: env("CLOUDINARY_KEY"),
+			api_secret: env("CLOUDINARY_SECRET"),
+		},
+		actionOptions: {
+			upload: {},
+			delete: {},
+		},
+	},
+}); */
 module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
-    },
-    actionOptions: {
-      upload: {},
-      delete: {},
-    },
-  },
-  // ...
+	upload: {
+		provider: "imagekit",
+		providerOptions: {
+			publicKey: env("IMAGE_KIT_PUBLIC_KEY"),
+			privateKey: env("IMAGE_KIT_PRIVATE_KEY"),
+			urlEndpoint: env("IMAGE_KIT_URL_ENDPOINT"),
+			// Optional
+			params: {
+				folder: "/portfolio",
+			},
+		},
+	},
 });
